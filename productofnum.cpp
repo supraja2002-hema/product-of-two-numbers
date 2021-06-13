e1f7b91f36c5355cb1843b8fa59141e9c299c80d
A. Hema Sai Supraja <82303022+supraja2002-hema@users.noreply.github.com>
2021-06-13 13:51:51 +0530

    Add files via upload

---
+++ productofnum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y;
+	double result;
+	cout<<"Enter x value:";
+	cin>>x;
+	cout<<"Enter y value:";
+	cin>>y;
+	result=x*y;
+	cout<<"The product of "<<x<<" and "<<y<<" is "<<result;
+	return 0;
+}
